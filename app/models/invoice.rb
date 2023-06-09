25b66d5b391d7a10a50e546134dd806517029c93
Michael Callahan <mcalla242@gmail.com>
2023-06-09 11:20:42 -0600

    Add relationships and testing for invoice -> coupon

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,6 +5,7 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  belongs_to :coupon, optional: true
 
   validates_presence_of :status
 
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items) }
     it { should have_many :transactions }
+    it { should belong_to(:coupon).optional }
   end
 
   context "validations" do
